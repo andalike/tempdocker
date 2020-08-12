3b50a02d4edd59e0629f7379097a0226297f3432
Ubuntu <ubuntu@ip-172-31-66-49.ec2.internal>
2020-08-12 02:20:25 +0000

    n times

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello, IfYou see this it worked second time {name}!</h3>" \
+    html = "<h3>Hello, If You see this it worked N time {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
