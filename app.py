0506c8e226fe857b19bdf5b751701063ea49171a
Ankit <43137695+andalike@users.noreply.github.com>
2020-08-12 07:49:08 +0530

    Changed to 2nd time

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>This means it works {name}!</h3>" \
+    html = "<h3>Hello, IfYou see this it worked second time {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
